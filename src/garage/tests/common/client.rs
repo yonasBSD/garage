7d05d9d5206e240702afb6340277ad075ecf5e97
Gwen Lg <me@gwenlg.fr>
2026-01-28 11:48:27 +0100

    chore: set aws_s3 BehaviorVersion to latest in tests common client
    ok because this code shouldn't be reliant on extremely specific behavior characteristics.
    And this avoid use of deprecated version.

--- src/garage/tests/common/client.rs
+++ src/garage/tests/common/client.rs
@@ -12,7 +12,7 @@ pub fn build_client(key: &Key) -> Client {
 		.endpoint_url(format!("http://127.0.0.1:{}", DEFAULT_PORT))
 		.region(super::REGION)
 		.credentials_provider(credentials)
-		.behavior_version(BehaviorVersion::v2024_03_28())
+		.behavior_version(BehaviorVersion::latest())
 		.build();
 
 	Client::from_conf(config)
