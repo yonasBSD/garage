c7d6eb631f1dddb0359678bba2a8dec6211a4550
Gwen Lg <me@gwenlg.fr>
2026-01-29 20:13:13 +0100

    chore: update cmd parameter name in shell.nix
    should fix the warning :
    WARN[0000] Flag --customPlatform is deprecated. Use: --custom-platform

--- shell.nix
+++ shell.nix
@@ -52,7 +52,7 @@ in
       function to_docker {
         executor  \
           --force \
-          --customPlatform="$(echo "''${DOCKER_PLATFORM}" | sed 's/i386/386/')" \
+          --custom-platform="$(echo "''${DOCKER_PLATFORM}" | sed 's/i386/386/')" \
           --destination "$(echo "''${CONTAINER_NAME}" | sed 's/i386/386/'):''${CONTAINER_TAG}" \
           --context dir://`pwd` \
           --verbosity=debug
